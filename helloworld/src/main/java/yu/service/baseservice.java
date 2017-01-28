9ed11fd74183b1d73739e23ef942515b82c9f094
yunan <598887996@qq.com>
2017-01-28 13:04:48 +0800

    2017-01-28 测试rxjava

---
+++ helloworld/src/main/java/yu/service/baseservice.java
@@ -0,0 +1,17 @@
+package yu.service;
+
+import io.vertx.core.Handler;
+import io.vertx.ext.sql.ResultSet;
+import io.vertx.ext.sql.SQLConnection;
+import rx.Observable;
+import yu.db.MysqlPool;
+
+/**
+ * Created by yunan on 2017/1/28.
+ */
+public class baseservice
+{
+    public static Observable<ResultSet> testquery(SQLConnection conn, String sql){
+        return MysqlPool.query(conn,sql);
+    }
+}
